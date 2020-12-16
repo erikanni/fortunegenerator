b0e3c321338375923314ca9c4216ea14dc1f3c27
erikanni <erik.anni@gmail.com>
2020-12-16 16:49:56 -0500

    spelling error

--- fortune_gen.py
+++ fortune_gen.py
@@ -10,7 +10,7 @@ action = (" will find", " will discover", " shall realize", " gain knowledge of"
 second_sub = (" an irishman" " your future", " meaning", " the world around you", " success", " the secret to success", " your future", " meaning", " the world around you", " success", " the secret to success", " your future", " meaning", " the world around you", " success", " the secret to success", " your future", "meaning", " the world around you", " success", " the secret to success", " your future", " meaning", " the world around you", " success", " the secret to success", )
 
 # <<<< STATEMENTS >>>>>
-subjectB = ("the irishamn", "Time", "Life", "Love", "the universe", "Time", "Life", "Love", "the universe", "Time", "Life", "Love", "the universe", "Time", "Life", "Love", "the universe",)
+subjectB = ("the irishman", "Time", "Life", "Love", "the universe", "Time", "Life", "Love", "the universe", "Time", "Life", "Love", "the universe", "Time", "Life", "Love", "the universe",)
 modo = (" has", " is", " does not" " have", " contains", " is not")
 modo2 = (" no", " much", " many", " too much")
 subjo = (" meaning", " happiness", " power", " influence")
